0c5cc62e7ba2ee19915b4b7e7ebf3b03631b05ab
Andrey Danil <adanilko@hawk.iit.edu>
2017-05-16 11:00:15 -0500

    adding youtube dashboard

---
+++ application/modules/Home/views/dashboard.php
@@ -0,0 +1,60 @@
+<div class="wrap">
+	<div class="section-dashboard">
+		<div class="box box-solid section-dashborad-head">
+            <div class="box-header with-border section-head">
+                <i class="fa fa-instagram" aria-hidden="true"></i> <?=l('list-instagram-tools')?>
+            </div>
+        </div>
+        <div class="section-list-dashboard">
+    		<div class="item">
+    			<a href="<?=PATH?>schedule" title="">
+	    			<img src="<?=BASE?>assets/img/icon-instagram-schedule.png" title="" alt="">
+	    			<div class="text"><?=l('auto-posts')?></div>
+    			</a>
+    		</div>
+            <div class="item">
+                <a href="<?=PATH?>instagram/direct-message" title="">
+                    <img src="<?=BASE?>assets/img/icon-instagram-auto-direct-message.png" title="" alt="">
+                    <div class="text"><?=l('auto-direct-message')?></div>
+                </a>
+            </div> 
+        	<div class="item">
+        		<a href="<?=PATH?>instagram/comment" title="">
+	    			<img src="<?=BASE?>assets/img/icon-instagram-comment.png" title="" alt="">
+	    			<div class="text"><?=l('auto-comments')?></div>
+	    		</a>
+    		</div>
+    		<div class="item">
+    			<a href="<?=PATH?>instagram/like" title="">
+	    			<img src="<?=BASE?>assets/img/icon-instagram-like.png" title="" alt="">
+	    			<div class="text"><?=l('auto-likes')?></div>
+	    		</a>
+    		</div>
+    		<div class="item">
+    			<a href="<?=PATH?>instagram/follow" title="">
+	    			<img src="<?=BASE?>assets/img/icon-instagram-follow.png" title="" alt="">
+	    			<div class="text"><?=l('auto-follow')?></div>
+	    		</a>
+    		</div>
+    		<div class="item">
+    			<a href="<?=PATH?>instagram/followback" title="">
+	    			<img src="<?=BASE?>assets/img/icon-instagram-followback.png" title="" alt="">
+	    			<div class="text"><?=l('auto-follow-back')?></div>
+	    		</a>
+    		</div>
+    		<div class="item">
+    			<a href="<?=PATH?>instagram/unfollow" title="">
+	    			<img src="<?=BASE?>assets/img/icon-instagram-unfollow.png" title="" alt="">
+	    			<div class="text"><?=l('auto-unfollow')?></div>
+	    		</a>
+    		</div>
+    		<div class="item">
+    			<a href="<?=PATH?>instagram/search" title="">
+	    			<img src="<?=BASE?>assets/img/icon-instagram-search.png" title="" alt="">
+	    			<div class="text"><?=l('search')?></div>
+	    		</a>
+    		</div>
+        	<div class="clearfix"></div>
+        </div>
+	</div>
+</div>
---
+++ application/modules/Home/views/footer.php
@@ -0,0 +1,37 @@
+<div class="section-4">
+	<div id="contact_us">
+		<div class="wrap">
+			<div class="section-title center"><?=l('contact-us')?></div>
+			<div class="section-desc center"><?=l('contact-us-desc')?></div>
+			<div class="info-us">
+				<div class="visit col-md-offset-2 col-md-4 col-xs-12">
+					<div class="icon"><i class="fa fa-map-marker"></i> <?=l('visit-us')?></div><br/>
+					<div class="mt7"><?=l('company-address')?></div><br/>
+				</div>
+				<div class="call call-end col-md-4 col-xs-12">
+					<div class="icon"><i class="fa fa-user"></i> <?=l('company-name')?></div><br/>
+					<div class="mt7"><?=l('company-fullname')?></div><br/>
+					<div><?=l('company-email')?></div><br/>
+					<div class="mt7"><?=l('company-phone')?></div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<div class="footer">
+	<div class="footer_content">
+		<div class="copyright book left"><?=l('copyright')?></div>
+		<div class="share_icon right book">
+			<span><?=l('share')?></span>
+			<a href="javascript:void(0);" title="" class="ml7">
+				<img src="<?=BASE?>assets/img/fb_icon.png" alt="" data-pin-nopin="true">
+			</a>
+			<a href="javascript:void(0);" title="">
+				<img src="<?=BASE?>assets/img/gg_icon.png" alt="">
+			</a>
+		</div>
+		<div class="clearAll"></div>
+	</div>
+</div>
