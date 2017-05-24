7f5e16b2c7f7384af5e8eaf3e4504cb5098df987
Andrey Danil <adanilko@hawk.iit.edu>
2017-05-23 21:50:00 -0500

    adding commenting

---
+++ application/modules/Instagram_comment/controllers/Instagram_comment.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Instagram_comment extends MX_Controller {
+	public function __construct(){
+		parent::__construct();
+		$this->load->model(get_class($this).'_model', 'model');
+	}
+
+	public function index(){
+		$accounts = $this->model->fetch("*", INSTAGRAM_ACCOUNT_TB, "uid = '".session("uid")."' AND status = 1", "created", "asc");
+		$schedule = $this->model->fetch("*", SCHEDULE_TB, "uid = '".session("uid")."' and schedule_type = 'comment'", "time_post", "desc");
+		$data = array(
+			'accounts' => $accounts,
+			'schedule' => $schedule
+		);
+		$this->template->title(TITLE);
+		$this->template->build('index', $data);
+	}
+}
