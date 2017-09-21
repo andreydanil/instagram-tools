2433789f03ef8b436570c2ec6f8fc34e9f40bde5
Andrey Danilkovich <andreydanilkovich@gmail.com>
2017-09-21 07:48:44 -0500

    Adding follower response and request

---
+++ vendor/mgp25/instagram-php/src/http/Response/FollowerResponse.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace InstagramAPI;
+
+class FollowerResponse extends Response
+{
+    public $followers;
+    public $next_max_id;
+
+    public function __construct($response)
+    {
+        if (self::STATUS_OK == $response['status']) {
+            $users = [];
+            foreach ($response['users'] as $user) {
+                $users[] = new User($user);
+            }
+            $this->followers = $users;
+            $this->next_max_id = isset($response['next_max_id']) ? $response['next_max_id'] : null;
+        } else {
+            $this->setMessage($response['message']);
+        }
+        $this->setStatus($response['status']);
+    }
+
+    /**
+     * @return User
+     */
+    public function getFollowers()
+    {
+        return $this->followers;
+    }
+
+    public function getNextMaxId()
+    {
+        return $this->next_max_id;
+    }
+}
